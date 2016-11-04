13039c23c96fde18d1eb09ef55d0939ce44ce8b1
Craig Ferry <PurityControl@users.noreply.github.com>
2016-11-04 20:08:15 +0000

    Gemfile and Rakefile now being created.

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,3 +1,13 @@
 var generators = require('yeoman-generator');
 
-module.exports = generators.Base.extend();
+module.exports = generators.Base.extend({
+  writing: {
+    Gemfile: function() {
+      this.copy('_Gemfile', 'Gemfile');
+    },
+    Rakefile: function(){
+      this.copy('_Rakefile', 'Rakefile');
+    }
+  }
+});
+
--- test/test-app.js
+++ test/test-app.js
@@ -4,18 +4,20 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
 
-describe('pc-simple-ruby-app', function(){
-   describe('default', function(){
-       before(function(done){
-           helpers.run(path.join(__dirname, '../generators/app'))
-                .on('end', done);
-       });
-       
-       it('creates files', function(){
-           assert.file([
-               'Gemfile',
-               'RakeFile'
-           ]);
-       });
-   });
+describe('pc simple ruby app', function(){
+  describe('default', function(){
+    before(function(done){
+      // on smaller dev machines this setup takes longer than the default 2s.
+      this.timeout(0);
+      helpers.run(path.join(__dirname, '../generators/app'))
+        .on('end', done);
+    });
+
+    it('creates files', function(){
+      assert.file([
+        'Gemfile',
+        'Rakefile'
+      ]);
+    });
+  });
 });
