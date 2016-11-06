efc9d890ddad0c92472c3291914c3ebc7df29208
Craig Ferry <PurityControl@users.noreply.github.com>
2016-11-06 15:57:56 +0000

    Generator expects project name argument.
    - Test and Project files named to project name
    - Content of Test and Project files refer to project name

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,10 +1,20 @@
 var generators = require('yeoman-generator');
+var _ = require('lodash');
 
 module.exports = generators.Base.extend({
   constructor: function() {
     generators.Base.apply(this, arguments);
 
     this.argument('project_name', {type: String, required: true});
+
+    var class_case = function(str) {
+      var firstLetter = str.charAt(0).toUpperCase();
+      var rest = str.slice(1);
+      return firstLetter + rest;
+    }
+
+    this.class_name = class_case(_.camelCase(this.project_name));
+    this.file_name = _.snakeCase(this.project_name);
   },
 
   writing: {
@@ -15,11 +25,21 @@ module.exports = generators.Base.extend({
       this.copy('_Rakefile', 'Rakefile');
     },
     Testfile: function() {
-      this.copy('test/_new_project_test.rb', 'test/new_project_test.rb');
+      this.fs.copyTpl(
+        this.templatePath('test/_new_project_test.rb'),
+        this.destinationPath('test/' + this.file_name + '_test.rb'),
+        {
+          class_name: this.class_name,
+          file_name: this.file_name
+        });
     },
     Rubyfile: function() {
-      this.copy('lib/_new_project.rb', 'lib/new_project.rb');
+      this.fs.copyTpl(
+        this.templatePath('lib/_new_project.rb'),
+        this.destinationPath('lib/' + this.file_name + '.rb'),
+       {
+         class_name: this.class_name
+       });
     }
   }
 });
-
--- generators/app/templates/lib/_new_project.rb
+++ generators/app/templates/lib/_new_project.rb
@@ -1,2 +1,2 @@
-class NewProject
+class <%= class_name %>
 end
